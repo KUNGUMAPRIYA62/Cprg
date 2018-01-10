ef15662b37aad7162bdc944724a69b0f79137f79
KUNGUMAPRIYA62 <35221138+KUNGUMAPRIYA62@users.noreply.github.com>
2018-01-10 21:52:41 +0530

    vowcons.C
    vowel or consonant

--- vowcons.C
+++ vowcons.C
@@ -1,15 +1,11 @@
 #include<stdio.h>
 int main()
 {
-char ch;  
-printf("Enter any character:");
-scanf("%c",&ch);
-if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
-{
-printf("'%c' is vowel.",ch);
-}
-else if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
-{
-printf("'%c' is consonant.",ch);
-}
+int year;  
+printf("Enter the year");
+scanf("%d",&year);
+if(((year%4==0)&&(year%100!==0))||(year%400==0))
+printf("%d is a leap year",year);
+else 
+printf("%d is not a leap year",year);
 }
