2c48097e4bf6b6ea29ee14acc306fefef509bc18
KUNGUMAPRIYA62 <35221138+KUNGUMAPRIYA62@users.noreply.github.com>
2018-01-12 16:50:43 +0530

    factorial.C
    factorial of a number

---
+++ factorial.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+int i,num;
+unsigned long long fact=1LL;
+printf("Enter any number to calculate factorial:");
+scanf("%d",&num);
+for(i=1;i<=num.i++)
+{
+fact=fact*i;
+}
+printf("Factorial of %d=%llu",num,fact);
+}
