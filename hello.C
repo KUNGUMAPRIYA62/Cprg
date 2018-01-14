d055cae28503f52735006ed10dc569b20e6d3b76
KUNGUMAPRIYA62 <35221138+KUNGUMAPRIYA62@users.noreply.github.com>
2018-01-14 20:16:35 +0530

    hello.C
    hello N times

--- hello.C
+++ hello.C
@@ -1,1 +1,12 @@
-
+#include<stdio.h>
+void main()
+{
+ int n;
+  {
+    printf("Enter number of times to printf 'HELLO'\n");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+      printf("HELLO");
+    }
+  }
