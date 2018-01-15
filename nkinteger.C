e07eb5ea9c1aa1a30c9068b79e60c5ab079e82c1
KUNGUMAPRIYA62 <35221138+KUNGUMAPRIYA62@users.noreply.github.com>
2018-01-15 13:50:57 +0530

    nkinteger.C
    sum of first K integer

---
+++ nkinteger.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int n,k;
+int i,a[n],sum=0;
+{
+printf("Enter the value of N");
+scanf("%d",&n);
+printf("Enter the values of K");
+scanf("%d",&k);
+}
+{
+printf("Enter the values of array");
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
+for(i=0;i<k;i++)
+sum=sum+a[i];
+printf("%d",sum);
+}
+}
