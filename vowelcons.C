e4ae9bf8175f7841fc302305d3f8a4e3e7b46004
KUNGUMAPRIYA62 <35221138+KUNGUMAPRIYA62@users.noreply.github.com>
2018-05-24 12:04:00 +0530

    vowelcons.C
    vowel consonant

---
+++ vowelcons.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{ 
+char ch;
+printf("Enter any character:");
+scanf("%c",&ch);
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+{
+printf("%c is vowel",ch);
+}
+else if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+{
+printf("%c is consonant",ch);
+}
+}
