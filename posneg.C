c7a76b69b8610cfc3e5239092c5bfa4b1155fada
KUNGUMAPRIYA62 <35221138+KUNGUMAPRIYA62@users.noreply.github.com>
2018-05-24 11:46:06 +0530

    posneg.C
    posneg

---
+++ posneg.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int num;
+printf("Enter any number:");
+scanf("%d",&num);
+if(num>0)
+{
+printf("Number is POSITIVE");
+}
+if(num<0)
+{
+printf("Number is NEGATIVE");
+}
+if(num==0)
+{
+printf("Number is ZERO");
+}
+}
